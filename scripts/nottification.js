c41121fcb1a16496cae72d89614d2645c24017f0
Yokay85 <voytynskyyda@gmail.com>
2025-03-16 23:28:51 +0200

    added ugly colors for blind, added bell shake

--- scripts/nottification.js
+++ scripts/nottification.js
@@ -1,12 +1,21 @@
 const bell = document.getElementById('bell');
 const notificationDot = document.getElementById('notificationDot');
 const notificationPopup = document.getElementById('notificationPopup');
-const bellIcon = document.getElementById('bellIcon');
+
+function addShakeEffect() {
+    if (notificationDot.style.display !== 'none') {
+        bell.classList.add('shake');
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    addShakeEffect();
+});
 
 bell.addEventListener('click', () => {
     window.open('messages.html', '_blank');
     notificationDot.style.display = 'none';
-
+    bell.classList.remove('shake')
 });
 
 bell.addEventListener('mouseover', () => {
@@ -14,5 +23,5 @@ bell.addEventListener('mouseover', () => {
 });
 
 bell.addEventListener('mouseout', () => {
-    notificationPopup.style.display = 'none';
-});
+    notificationPopup.style.display = 'none'}
+);
